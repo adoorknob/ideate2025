ef7715ea03b9a756460baf6b388d8a3fe0e82755
adoorknob <quantengwai@gmail.com>
2025-06-11 19:09:18 +0800

    add what is ideate

--- src/components/whatIsIdeate/WhatIsIdeate.tsx
+++ src/components/whatIsIdeate/WhatIsIdeate.tsx
@@ -1,10 +1,25 @@
 import React from "react"
+import TitleText from "./title_text"
 
 const WhatIsIdeate: React.FC = () => {
     return (
-        <div className="bg-inherit flex items-center justify-center text-black text-base h-screen">
-            <div className="rounded-md p-5 text-center bg-gray-300">
-                What is IDEATE?
+        <div className="bg-inherit flex flex-col items-center text-white justify-center w-full text-base h-screen">
+            <div className="relative w-full px-20 h-48">
+                <TitleText />
+            </div>
+            <div className="w-full px-20 grid grid-cols-3 gap-4 gap-y-10">
+                <div className="col-span-2 text-3xl text-left">
+                    blah blah blah history of ideate blah blah blah history of ideate blah blah blah history of ideate
+                </div>
+                <div className="bg-zinc-700 content-center">
+                    insert some image
+                </div>
+                <div className="bg-zinc-700 content-center">
+                    insert some image
+                </div>
+                <div className="col-span-2 text-3xl text-left">
+                    blah blah blah history of ideate blah blah blah history of ideate blah blah blah history of ideate
+                </div>
             </div>
         </div>
     )
---
+++ src/components/whatIsIdeate/title_text.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const TitleText: React.FC = () => {
+    return (
+        <div className="font-extrabold uppercase text-white md:text-5xl lg:text-7xl">
+            <p className="absolute top-10 h-auto">
+                What is
+            </p>
+            <p className="absolute md:top-20 lg:top-25 md:left-60 lg:left-81 h-auto">
+                IDEATE?
+            </p>
+        </div>
+    )
+}
+
+export default TitleText
