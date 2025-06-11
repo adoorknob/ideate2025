f8eeff6e573a8cf76adc56f244f2a55c46b1658d
adoorknob <quantengwai@gmail.com>
2025-06-11 17:46:22 +0800

    add title card and favicon

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,9 +3,7 @@ import React from "react"
 const Hero: React.FC = () => {
     return (
         <div className="bg-inherit flex items-center justify-center text-black text-base h-screen">
-            <div className="rounded-md p-5 text-center bg-gray-300">
-                Hero
-            </div>
+            <img src="../logo/main_logo.png" />
         </div>
     )
 }
