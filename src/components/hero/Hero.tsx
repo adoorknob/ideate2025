d3fa258f00125826a6c9e9818e31e3eaa15fdca9
adoorknob <quantengwai@gmail.com>
2025-06-13 16:14:44 +0800

    add partners

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,7 +3,10 @@ import React from "react"
 const Hero: React.FC = () => {
     return (
         <div className="bg-inherit flex items-center justify-center text-black text-base h-screen">
-            <img src="../logo/main_logo.png" />
+            <img 
+                src="../logo/main_logo.png" 
+                className="w-[60vw] h-auto m-20"
+            />
         </div>
     )
 }
--- src/components/sponsor/Sponsor.tsx
+++ src/components/sponsor/Sponsor.tsx
@@ -2,9 +2,48 @@ import React from "react"
 
 const Sponsor: React.FC = () => {
     return (
-        <div className="bg-inherit flex items-center justify-center text-black text-base h-screen">
-            <div className="rounded-md p-5 text-center bg-gray-300">
-                Sponsor
+        <div className="bg-inherit items-center text-white w-full text-base min-h-screen">
+            <div className="uppercase font-bold m-20">
+                <div className="flex relative text-7xl text-left w-full h-35 my-5">
+                    <div>
+                        co-
+                    </div>
+                    <div className="absolute text-nowrap left-14 top-15">
+                        organised by:
+                    </div>
+                </div>
+                <div className="w-full grid grid-cols-2 content-center items-center justify-between bg-white rounded-3xl">
+                    <img 
+                        src="../sponsors/cde_logo.png"
+                        className="w-[25vw] m-10 self-center justify-self-center"
+                    />
+                    <img 
+                        src="../sponsors/enterprise.png"
+                        className="w-[30vw] m-5 justify-self-center"
+                    />
+                    <img 
+                        src="../sponsors/idpsc.avif"
+                        className="w-[10vw] m-5 justify-self-center col-span-2"
+                    />
+                </div>
+                <div className="flex relative text-7xl text-left w-full h-35 my-5">
+                    <div>
+                        supported
+                    </div>
+                    <div className="absolute text-nowrap left-92 top-15">
+                        by:
+                    </div>
+                </div>
+                <div className="w-full grid grid-cols-2 content-center items-center justify-between bg-white rounded-3xl">
+                    <img 
+                        src="../sponsors/cde_logo.png"
+                        className="w-[25vw] m-10 self-center justify-self-center"
+                    />
+                    <img 
+                        src="../sponsors/enterprise.png"
+                        className="w-[30vw] m-5 justify-self-center"
+                    />
+                </div>
             </div>
         </div>
     )
