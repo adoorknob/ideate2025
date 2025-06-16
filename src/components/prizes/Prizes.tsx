fb899698202135ee889501a843ca86c9de94931d
adoorknob <quantengwai@gmail.com>
2025-06-16 11:15:12 +0800

    add some stuff for prizes

--- src/components/prizes/Prizes.tsx
+++ src/components/prizes/Prizes.tsx
@@ -2,9 +2,23 @@ import React from "react"
 
 const Prizes: React.FC = () => {
     return (
-        <div className="bg-inherit flex items-center justify-center text-black text-base h-screen">
-            <div className="rounded-md p-5 text-center bg-gray-300">
-                Prizes
+        <div className="bg-inherit flex flex-col items-center justify-center text-white uppercase text-base h-screen">
+            <div className="text-7xl font-extrabold">
+                prizes:
+            </div>
+            <div className="grid grid-cols-3 place-content-center">
+                <img 
+                    src="../prizes/amongsemis_label_text.png"
+                    className="w-[50vw] justify-self-center"
+                />
+                <img 
+                    src="../prizes/best_exhibition.png"
+                    className="w-[25vw] justify-self-center"
+                />
+                <img 
+                    src="../prizes/most_sustainable.png"
+                    className="w-[25vw] justify-self-center"
+                />
             </div>
         </div>
     )
