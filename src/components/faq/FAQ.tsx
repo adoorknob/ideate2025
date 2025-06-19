fb2e1c0c5e7a319b6b493095facb6dbfbbe5b291
adoorknob <quantengwai@gmail.com>
2025-06-19 11:59:10 +0800

    add NOC faq qn

--- src/components/faq/FAQ.tsx
+++ src/components/faq/FAQ.tsx
@@ -1,11 +1,83 @@
 import React from "react"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+const faqData = [
+  {
+    question: "Is it accessible?",
+    answer: (
+      <span>
+        Yes. It adheres to the{" "}
+        <a
+          href="https://www.w3.org/WAI/ARIA/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 underline hover:text-blue-800"
+        >
+          WAI-ARIA design pattern
+        </a>
+        .
+      </span>
+    ),
+  },
+  {
+    question: "Can I use it in React?",
+    answer: (
+      <span>
+        Absolutely! It works seamlessly with <strong>React</strong> and{" "}
+        <a href="https://tailwindcss.com" className="text-blue-600 underline">
+          Tailwind CSS
+        </a>
+        .
+      </span>
+    ),
+  },
+  {
+    question: "What is NOC Greenlane?",
+    answer: (
+      <span>
+        <a 
+            href="https://enterprise.nus.edu.sg/education-programmes/nus-overseas-colleges/" 
+            className="underline hover:text-gray-200 font-semibold">
+                NUS Overseas Colleges (NOC)
+        </a> 
+        {" "} is an overseas programme that provides experiential learning and entrpreneurship development, with access to
+        over 25 global innovative hotspots. 
+        <br />
+        <br />
+        The NOC Greenlane provided by IDEATE 2025 will allow participants to skip the interview process. However, do take note
+        that participants will still have to meet all application requirements set forth by NOC. Students will still have to 
+        apply for NOC and select 3 choices. NOC has the right to consider other locations for participants besides 
+        their 3 choices. 
+        <br />
+        <br />
+        Students will also have to meet other requirements such as securing a company and securing a visa before embarking on
+        the programme. 
+      </span>
+    ),
+  },
+];
+
 
 const FAQ: React.FC = () => {
     return (
-        <div className="bg-inherit flex items-center justify-center text-black text-base h-screen">
-            <div className="rounded-md p-5 text-center bg-gray-300">
-                FAQ
+        <div className="bg-inherit items-center justify-center text-white text-xl px-15 md:px-20 lg:px-50 pt-20">
+            <div className="uppercase text-5xl md:text-6xl lg:text-7xl font-extrabold py-10">
+                faq
             </div>
+
+            <Accordion className="bg-gray-100/10 px-5 rounded-xl" type="single" collapsible>
+            {faqData.map((item, index) => (
+                <AccordionItem key={index} value={`item-${index}`}>
+                <AccordionTrigger className="text-lg md:text-xl lg:text-2xl">{item.question}</AccordionTrigger>
+                <AccordionContent className="text-justify text-base md:text-lg lg:text-xl">{item.answer}</AccordionContent>
+                </AccordionItem>
+            ))}
+            </Accordion>
         </div>
     )
 }
