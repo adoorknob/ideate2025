3e5287946fdb42d8da4168882e2a30e0c1037f2b
adoorknob <quantengwai@gmail.com>
2025-07-25 13:30:14 +0800

    automate registration close

--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -1,10 +1,15 @@
 import React from "react"
 import RegistrationBulb from "../registerNow/RegistrationBulb"
 
-const Hero: React.FC = () => {
+type HeroProps = {
+    isRegistrationOpen: boolean;
+};
+
+const Hero: React.FC<HeroProps> = ({ isRegistrationOpen }) => {
     return (
         <div className="bg-inherit flex items-center justify-center text-black text-base min-h-screen">
             <div className="relative w-[70vw] md:w-[45vw] aspect-[1/1]">
+            { isRegistrationOpen ? 
                 <div className="
                             absolute 
                             w-22/100 
@@ -14,6 +19,17 @@ const Hero: React.FC = () => {
                             transform -translate-x-1/2 translate-y-full">
                     <RegistrationBulb />
                 </div>
+                :
+                <div className="
+                            absolute 
+                            w-22/100 
+                            h-auto
+                            z-10
+                            left-[38%] -top-[20%]
+                            transform -translate-x-1/2 translate-y-full">
+                    <img src="../register_now/lit_bulb.png"/>
+                </div>
+            }
                 <img 
                     // src="../logo/main_logo.png" 
                     src="../logo/logo_wo_bulb.png" 
