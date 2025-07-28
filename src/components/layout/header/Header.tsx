9e938a21a0e12c3695fd29596a485b68555905ca
adoorknob <quantengwai@gmail.com>
2025-07-28 14:27:51 +0800

    fix order of buttons

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -50,8 +50,8 @@ const Header: React.FC = () => {
         >
           {/* Routing done by putting url in route */}
           <HeaderButton buttonName="Home" route="/" onClick={turnOffSidebar}/>
-          <HeaderButton buttonName="Contact Us" route="/about" onClick={turnOffSidebar}/>
           <HeaderButton buttonName="Timeline" route="/schedule" onClick={turnOffSidebar}/>
+          <HeaderButton buttonName="Contact Us" route="/about" onClick={turnOffSidebar}/>
           <HeaderButton buttonName="FAQ" route="/faq" onClick={turnOffSidebar}/>        
           {/* <HeaderButton buttonName="Home" route="/a1b2c3d4/" onClick={turnOffSidebar}/>
           <HeaderButton buttonName="Timeline" route="/a1b2c3d4/schedule" onClick={turnOffSidebar}/>
